eedcecc8e5864cbf55387f0598ca14f347aba710
jeremy-2003 <62605238+jeremy-2003@users.noreply.github.com>
2022-10-20 14:49:46 -0500

    Estructura

---
+++ src/main/java/pe/com/babelfarma/babelfarmabackend/BabelfarmaBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.com.babelfarma.babelfarmabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabelfarmaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabelfarmaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/com/babelfarma/babelfarmabackend/BabelfarmaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.babelfarma.babelfarmabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabelfarmaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
