2a9ee7879d0f1ba3f53382b84af2cf7646503187
Yacori Lucero <u20201b700.@upc.edu.pe>
2022-10-21 23:13:52 -0500

    Pricila Lucero

--- src/main/java/pe/com/babelfarma/babelfarmabackend/entities/Cliente.java
+++ src/main/java/pe/com/babelfarma/babelfarmabackend/entities/Cliente.java
@@ -1,4 +1,97 @@
 package pe.com.babelfarma.babelfarmabackend.entities;
 
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name="cliente")
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int dni;
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String correo;
+    private int celular;
+    private Date fechaNacimiento;
+
+    public Cliente() {
+    }
+
+    public Cliente(int dni, String nombres, String apellidoPaterno, String apellidoMaterno, String correo, int celular, Date fechaNacimiento) {
+        this.dni = dni;
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.correo = correo;
+        this.celular = celular;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
--- src/main/java/pe/com/babelfarma/babelfarmabackend/entities/Envio.java
+++ src/main/java/pe/com/babelfarma/babelfarmabackend/entities/Envio.java
@@ -1,4 +1,44 @@
 package pe.com.babelfarma.babelfarmabackend.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="envio")
 public class Envio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String encargado;
+    private String direccionEnvio;
+
+    public Envio(){
+    }
+    public Envio(String encargado, String direccionEnvio) {
+        this.encargado = encargado;
+        this.direccionEnvio = direccionEnvio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEncargado() {
+        return encargado;
+    }
+
+    public void setEncargado(String encargado) {
+        this.encargado = encargado;
+    }
+
+    public String getDireccionEnvio() {
+        return direccionEnvio;
+    }
+
+    public void setDireccionEnvio(String direccionEnvio) {
+        this.direccionEnvio = direccionEnvio;
+    }
 }
