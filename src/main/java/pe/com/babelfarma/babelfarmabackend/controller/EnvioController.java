e7ebb53a03f7f104a480def3ffed9d68ff5d4472
DIEGO <u20201a152@upc.edu.pe>
2022-10-23 10:56:02 -0500

    Barja's Fifth Commit 😴

--- src/main/java/pe/com/babelfarma/babelfarmabackend/controller/EnvioController.java
+++ src/main/java/pe/com/babelfarma/babelfarmabackend/controller/EnvioController.java
@@ -19,6 +19,16 @@ public class EnvioController {
         List<Envio> envios = envioRepository.findAll();
         return new ResponseEntity<List<Envio>>(envios, HttpStatus.OK);
     }
+    @GetMapping("/envios/{id}")
+    public ResponseEntity<Envio> findById(@PathVariable("id") Long id){
+        Envio envio = envioRepository.findByIdJPQL(id);
+        return new ResponseEntity<Envio>(envio, HttpStatus.OK);
+    }
+    @GetMapping("/envios/{direccion}")
+    public ResponseEntity<List<Envio>> findByDireccionEnvioJPQL(@PathVariable("direccion") String direccion){
+        List<Envio> envios = envioRepository.findByDireccionEnvioJPQL(direccion);
+        return new ResponseEntity<List<Envio>>(envios, HttpStatus.OK);
+    }
     @PostMapping("/envios")
     public ResponseEntity<Envio> createEnvio(@RequestBody Envio envio){
         Envio newEnvio =
@@ -31,5 +41,22 @@ public class EnvioController {
                 )
         );
         return new ResponseEntity<Envio>(newEnvio, HttpStatus.CREATED);
+    }
+    @PutMapping("/envios")
+    public ResponseEntity<Envio> updateEnvio(
+            @PathVariable("id") Long id,
+            @RequestBody Envio envio){
+        Envio envioUpdate = envioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No se encontró el envío con id: " + id));
+        envioUpdate.setNombreEncargado(envio.getNombreEncargado());
+        envioUpdate.setApellidoEncargado(envio.getApellidoEncargado());
+        envioUpdate.setCelular(envio.getCelular());
+        envioUpdate.setDireccionEnvio(envio.getDireccionEnvio());
+        return new ResponseEntity<Envio>(envioRepository.save(envioUpdate), HttpStatus.OK);
+    }
+    @DeleteMapping("/envios/{id}")
+    public ResponseEntity<HttpStatus> deleteEnvio(@PathVariable("id") Long id){
+        envioRepository.deleteById(id);
+        return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/pe/com/babelfarma/babelfarmabackend/repository/EnvioRepository.java
+++ src/main/java/pe/com/babelfarma/babelfarmabackend/repository/EnvioRepository.java
@@ -1,12 +1,15 @@
 package pe.com.babelfarma.babelfarmabackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pe.com.babelfarma.babelfarmabackend.entities.Envio;
 
 import java.util.List;
 
 public interface EnvioRepository
     extends JpaRepository<Envio, Long> {
-        List<Envio> findById(long id);
-        List<Envio> findByDireccionEnvio(String direccionEnvio);
+        @Query("SELECT e FROM Envio e WHERE e.id=?1")
+        Envio findByIdJPQL(long id);
+        @Query("SELECT e FROM Envio e WHERE e.direccionEnvio=?1")
+        List<Envio> findByDireccionEnvioJPQL(String direccionEnvio);
 }
