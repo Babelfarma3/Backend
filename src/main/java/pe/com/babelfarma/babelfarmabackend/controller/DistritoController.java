2b8af7ac3afc732b752d2f748ac708f1b7d722cc
DIEGO <u20201a152@upc.edu.pe>
2022-10-23 16:15:34 -0500

    PostMapping Distrito

--- src/main/java/pe/com/babelfarma/babelfarmabackend/controller/DistritoController.java
+++ src/main/java/pe/com/babelfarma/babelfarmabackend/controller/DistritoController.java
@@ -3,9 +3,7 @@ package pe.com.babelfarma.babelfarmabackend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pe.com.babelfarma.babelfarmabackend.entities.Distrito;
 import pe.com.babelfarma.babelfarmabackend.repository.DistritoRepository;
 
@@ -20,6 +18,15 @@ public class DistritoController {
     private ResponseEntity<List<Distrito>> getAllDistritos(){
         List<Distrito> distritos = distritoRepository.findAll();
         return new ResponseEntity<List<Distrito>>(distritos, HttpStatus.OK);
+    }
+    @PostMapping("/distritos")
+    private ResponseEntity<Distrito> createDistrito(@RequestBody Distrito distrito){
+        Distrito newDistrito =
+                distritoRepository.save(new Distrito(
+                        distrito.getNombreDistrito()
+                )
+        );
+        return new ResponseEntity<Distrito>(newDistrito, HttpStatus.CREATED);
     }
 
 }
