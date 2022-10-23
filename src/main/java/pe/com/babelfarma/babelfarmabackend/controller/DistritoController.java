cf14e88db57ef31dd5089a039c3fb49295e27410
DIEGO <u20201a152@upc.edu.pe>
2022-10-23 16:11:10 -0500

    GetMapping Distrito

--- src/main/java/pe/com/babelfarma/babelfarmabackend/controller/DistritoController.java
+++ src/main/java/pe/com/babelfarma/babelfarmabackend/controller/DistritoController.java
@@ -1,4 +1,25 @@
 package pe.com.babelfarma.babelfarmabackend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.com.babelfarma.babelfarmabackend.entities.Distrito;
+import pe.com.babelfarma.babelfarmabackend.repository.DistritoRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class DistritoController {
+    @Autowired
+    private DistritoRepository distritoRepository;
+    @GetMapping("/distritos")
+    private ResponseEntity<List<Distrito>> getAllDistritos(){
+        List<Distrito> distritos = distritoRepository.findAll();
+        return new ResponseEntity<List<Distrito>>(distritos, HttpStatus.OK);
+    }
+
 }
